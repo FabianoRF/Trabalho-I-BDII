60b9a079b455171b6f8931c296e489dafffce6fe
Carlos Padilha <cpadilha38@outlook.com.br>
2020-06-01 15:00:42 -0300

    criaçao da classe Players

--- Projeto teste redis/src/main/MesaDePoker.java
+++ Projeto teste redis/src/main/MesaDePoker.java
@@ -189,25 +189,4 @@ public class MesaDePoker {
         }
     }
 
-    public class Players{
-        private String nome;  // nome do jogador
-        private int stack; // numero de fichas que ele possui
-        private boolean smallBlind; // marca se ele é small blid nessa mão
-        private boolean bigBlind; // marca se ele é big blid nessa mão
-
-        public Players(String nome, int stack) {
-            this.nome = nome;
-            this.stack = stack;
-            this.smallBlind = false;
-            this.bigBlind = false;
-        }
-
-        @Override
-        public String toString() {
-            return "Players{" +
-                    "nome='" + nome + '\'' +
-                    ", stack=" + stack +
-                    '}';
-        }
-    }
 }
