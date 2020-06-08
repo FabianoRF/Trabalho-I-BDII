87ca52b6066e692da63fecc05ef9364e5b2603e2
FabianoRF <freis884@gmail.com>
2020-06-08 10:25:37 -0300

    teste

--- Projeto teste redis/src/main/Main.java
+++ Projeto teste redis/src/main/Main.java
@@ -7,7 +7,9 @@ import java.util.List;
 import java.util.Random;
 
 public class Main {
+    
     private static final int NUMERO_DE_INSCRITOS = 900;
+
     public static void main(String[] args) {
 
         try {
@@ -32,10 +34,7 @@ public class Main {
             for (int i=0;i<lista1.size();i++){
                 System.out.println("Imprimindo lista: " + lista1.get(i));
             }
-
-
-
-
+            
         }catch(Exception e) {
             System.out.println(e);
         }
@@ -48,6 +47,7 @@ public class Main {
         pegarTodosParticipantes(playersNoCampeonato);
         int i, j, tamanhoDoPote, valorDoBB = 20, valorDoSB = 10; // numero de fichas do pote e tamanho do big blind e small blind no momento
         int valorAposta; // variavel que armazena o valor co 2bet
+
         while (playersNoCampeonato.size() > 1){
             encherMesa(playersNoCampeonato, playersNaMesa);
             while (playersNaMesa.size() > 1) {
